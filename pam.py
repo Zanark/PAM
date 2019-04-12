62d1aa1e7b326a23cd028923de9f953f922dedfa
Zanark <kiodeba11@gmail.com>
2019-04-12 21:35:01 +0530

    mic demo

--- pam.py
+++ pam.py
@@ -3,12 +3,15 @@ import speech_recognition as sr
 #print(sr.__version__)
 
 r = sr.Recognizer()
+mic = sr.Microphone()
+#sr.Microphone.list_microphone_names()
 
-with sr.Microphone() as src:
+with mic as src:
+    print("\n\nbol")
     audio = r.listen(src)
     print("sun liya")
 
 try:
-    print("Recognized o/p: "+r.recognize_google(audio))
+    print("\n\nRecognized o/p: "+r.recognize_google(audio))
 except:
     pass
