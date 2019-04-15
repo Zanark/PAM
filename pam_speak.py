22f72aa3b32b57cae6acf75b089db7b0fc4d1a41
Zanark <kiodeba11@gmail.com>
2019-04-15 22:28:06 +0530

    giving a voice to pam

---
+++ pam_speak.py
@@ -0,0 +1,10 @@
+import pyttsx3 as tts
+
+engine = tts.init()
+voices = engine.getProperty('voices')
+for voice in voices:
+   engine.setProperty('voice', voice.id)
+   engine.say('The quick brown fox jumped over the lazy dog.')
+engine.runAndWait()
+
+
