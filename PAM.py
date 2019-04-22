2174a8fd7446cc5501790c6a881595f13ac48f61
Zanark <kiodeba11@gmail.com>
2019-04-22 20:35:02 +0530

    ambient noise 0.5

--- PAM.py
+++ PAM.py
@@ -12,7 +12,7 @@ r.energy_threshold = 350
 while True:
     with mic as source:
         print("\n\nbol")
-        r.adjust_for_ambient_noise(source, duration=0.4) # we only need to calibrate once, before we start listening
+        r.adjust_for_ambient_noise(source, duration=0.5) # we only need to calibrate once, before we start listening
         audio = r.listen(source, timeout=5, phrase_time_limit=7)
         tasks(r, audio)
         print("\nsun liya")
